fbefc2e97422e572284a9efa3674792175ac254a
krishnatejab <krishnatejab@turito.com>
2023-11-05 17:18:18 +0530

    popup styles added

--- src/charts/bar.js
+++ src/charts/bar.js
@@ -2,17 +2,106 @@ import React, { useEffect,useState } from "react";
 import * as d3 from "d3";
 import { useD3 } from "../hooks/d3hook";
 
-export const Bar = (props) => {
+// export const Bar = (props) => {
+//   const [data,setData] = useState(props.barData)
+
+//   useEffect(()=>{
+//     setData(props.barData)
+//   })
+//   const ref = useD3(
+//     (svg) => {
+//       const height = 300;
+//       const width = 500;
+//       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+//       const x = d3
+//         .scaleBand()
+//         .domain(data.map((d) => d.year))
+//         .rangeRound([margin.left, width - margin.right])
+//         .padding(0.1);
+
+//       const y1 = d3
+//         .scaleLinear()
+//         .domain([0, d3.max(data, (d) => d.sales)])
+//         .rangeRound([height - margin.bottom, margin.top]);
+
+//       const xAxis = (g) =>
+//         g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+//           d3
+//             .axisBottom(x)
+//             .tickValues(
+//               d3
+//                 .ticks(...d3.extent(x.domain()), width / 40)
+//                 .filter((v) => x(v) !== undefined)
+//             )
+//             .tickSizeOuter(0)
+//         );
+
+//       const y1Axis = (g) =>
+//         g
+//           .attr("transform", `translate(${margin.left},0)`)
+//           .style("color", "steelblue")
+//           .call(d3.axisLeft(y1).ticks(null, "s"))
+//           .call((g) => g.select(".domain").remove())
+//           .call((g) =>
+//             g
+//               .append("text")
+//               .attr("x", -margin.left)
+//               .attr("y", 10)
+//               .attr("fill", "currentColor")
+//               .attr("text-anchor", "start")
+//               .text(data.y1)
+//           );
+
+//       svg.select(".x-axis").call(xAxis);
+//       svg.select(".y-axis").call(y1Axis);
+
+//       svg
+//         .select(".plot-area")
+//         .attr("fill", "green")
+//         .selectAll(".bar")
+//         .data(data)
+//         .join("rect")
+//         .attr("class", "bar")
+//       .attr("x", (d) => x(d.year))
+//       .attr("width", x.bandwidth())
+//       .attr("y", (d) => y1(d.sales))
+//       .attr("height", (d) => y1(0) - y1(d.sales));
+//         // .attr("x", (d, i) => i * (width / data.length))
+//         // .attr("y", (d) => height - d * (height / 100))
+//         // .attr("width", 0.8 * (width / data.length))
+//         // .attr("height", (d) => d * (height / 100));
+//     },
+//     [data.length]
+//   );
+//   // should use useEffect
+//   return (
+//     <svg
+//       ref={ref}
+//       className="svgWidth"
+//       style={{
+//         height: "45vh",
+//         width: "90%",
+//         marginRight: "0px",
+//         marginLeft: "0px",
+//       }}
+//     >
+//       <g className="plot-area" />
+//       <g className="x-axis" />
+//       <g className="y-axis" />
+//     </svg>
+//   );
+// };
+const height = 500;
+const width = 500;
+export const Bar = ( props ) =>{
   const [data,setData] = useState(props.barData)
-  console.log(data)
 
   useEffect(()=>{
     setData(props.barData)
   })
   const ref = useD3(
     (svg) => {
-      const height = 300;
-      const width = 500;
       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
       const x = d3
@@ -57,6 +146,9 @@ export const Bar = (props) => {
       svg.select(".x-axis").call(xAxis);
       svg.select(".y-axis").call(y1Axis);
 
+      var tooltip = d3.select('.tooltip-area')
+        .style('opacity', 0);
+
       svg
         .select(".plot-area")
         .attr("fill", "green")
@@ -64,31 +156,34 @@ export const Bar = (props) => {
         .data(data)
         .join("rect")
         .attr("class", "bar")
-      .attr("x", (d) => x(d.year))
-      .attr("width", x.bandwidth())
-      .attr("y", (d) => y1(d.sales))
-      .attr("height", (d) => y1(0) - y1(d.sales));
-        // .attr("x", (d, i) => i * (width / data.length))
-        // .attr("y", (d) => height - d * (height / 100))
-        // .attr("width", 0.8 * (width / data.length))
-        // .attr("height", (d) => d * (height / 100));
+        .attr("x", (d) => x(d.year))
+        .attr("width", x.bandwidth())
+        .attr("y", (d) => y1(d.sales))
+        .attr("height", (d) => y1(0) - y1(d.sales))
+        .append('title')
+        .text((d) => `Sales were ${d.sales} in ${d.year}`);
     },
     [data.length]
   );
-  // should use useEffect
+
   return (
-    <svg
-      ref={ref}
-      style={{
-        height: "45vh",
-        width: "90%",
-        marginRight: "0px",
-        marginLeft: "0px",
-      }}
-    >
-      <g className="plot-area" />
-      <g className="x-axis" />
-      <g className="y-axis" />
-    </svg>
+    <>
+      <svg
+        ref={ref}
+        viewBox={`0 0 ${height} ${width}`}
+        style={{
+          height: "100%",
+          marginRight: "0px",
+          marginLeft: "0px",
+        }}
+      >
+        <g className="plot-area" />
+        <g className="x-axis" />
+        <g className="y-axis" />
+        <g className="tooltip-area">
+          <text className="tooltip-area__text">aas</text>
+        </g>
+      </svg>
+    </>
   );
-};
+}
--- src/components/home.js
+++ src/components/home.js
@@ -5,45 +5,70 @@ import Line from "../charts/line";
 import "./home.css";
 
 //bootstrap cards
-import { Card, CardContent, CardActions,Button } from "@mui/material";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
+import { Card, CardContent, CardActions, Button } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+
+//table imports from material ui
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-
 class Home extends React.Component {
   constructor(props) {
     super();
-
+    
     this.state = {
       barData: [],
       lineData: [],
       months: [
-        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
       ],
-      open:false
+      open: false,
+      rows: [
+        this.createData('Sales', this.randomData(Math.random(),Math.random()),this.randomData(Math.random(),Math.random())),
+        this.createData('Advertising', this.randomData(Math.random(),Math.random()),this.randomData(Math.random(),Math.random())),
+        this.createData('Inventory', this.randomData(Math.random(),Math.random()),this.randomData(Math.random(),Math.random())),
+        this.createData('Entertainment', this.randomData(Math.random(),Math.random()),this.randomData(Math.random(),Math.random())),
+        this.createData('Product', this.randomData(Math.random(),Math.random()),this.randomData(Math.random(),Math.random())),
+      ]
     };
   }
   componentDidMount() {
     this.setState({
       lineData: [],
-      barData:[]
+      barData: [],
     });
     this.updateData();
   }
 
   handleClick = () => {
-   this.setState((prevState) =>({
-    open: prevState.open == false ? true : false
-   }))
-   console.log(this.state.open)
+    this.setState((prevState) => ({
+      open: prevState.open === false ? true : false,
+    }));
   };
 
   getRandomNumbers() {
@@ -74,93 +99,185 @@ class Home extends React.Component {
       barData: this.getRandomNumbersBardata(),
     });
     // console.log(this.state.lineData)
-  }
+  };
   randomNumber(min, max) {
     let mx = max > min ? max : min;
     let mn = max < min ? max : min;
     return Math.floor((Math.random() * (mx - mn) + mn) * 100);
   }
 
+  randomData(min, max) {
+    let mx = max > min ? max : min;
+    let mn = max < min ? max : min;
+    return Math.round((Math.random() * (mx - mn) + mn) * 100);
+  }
+
+  createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+
   render() {
     return (
       <div className="home-div">
-        <Card style={{width: "40%",margin:"2vw"}}>
-          <CardActions style={{justifyContent:"space-between"}}>
+        <Card style={{ width: "40%", margin: "2vw" }}>
+          <CardActions style={{ justifyContent: "space-between" }}>
             <p>Checking Account</p>
-            <div style={{justifyContent:"space-between"}}>
-              <Button variant="outlined" style={{marginRight:"3px",borderColor:"ButtonHighlight",color:"green"}} onClick={this.updateData}>Manage</Button>
-               <select onChange={this.updateData} style={{marginRight:"3px",borderColor:"ButtonHighlight",color:"green"}}>
-                {this.state.months.map((e)=> (<option value={e}> {e}</option>))}
-               </select>
+            <div style={{ justifyContent: "space-between" }}>
+              <Button
+                variant="outlined"
+                style={{
+                  marginRight: "3px",
+                  borderColor: "ButtonHighlight",
+                  color: "green",
+                }}
+                onClick={this.updateData}
+              >
+                Manage
+              </Button>
+              <select
+                onChange={this.updateData}
+                style={{
+                  marginRight: "3px",
+                  borderColor: "ButtonHighlight",
+                  color: "green",
+                }}
+              >
+                {this.state.months.map((e) => (
+                  <option value={e}> {e}</option>
+                ))}
+              </select>
             </div>
           </CardActions>
           <hr />
-          <CardContent style={{paddingLeft:"50px",alignItems:"center"}}>
-          <Line lineData={this.state.lineData} />
+          <CardContent style={{ paddingLeft: "50px", alignItems: "center" }}>
+            <Line lineData={this.state.lineData} />
           </CardContent>
         </Card>
-        <Card style={{width: "40%" ,margin:"2vw"}}>
-          <CardActions style={{justifyContent:"space-between"}}>
+        <Card style={{ width: "40%", margin: "2vw" }}>
+          <CardActions style={{ justifyContent: "space-between" }}>
             <p>Invoices owed to you</p>
-            <Button variant="outlined" style={{marginRight:"3px",borderColor:"ButtonHighlight",color:"green"}} onClick={this.handleClick}> New Sales Invoice</Button>
+            <Button
+              variant="outlined"
+              style={{
+                marginRight: "3px",
+                borderColor: "ButtonHighlight",
+                color: "green",
+              }}
+              onClick={this.handleClick}
+            >
+              {" "}
+              New Sales Invoice
+            </Button>
           </CardActions>
           <hr />
-          <CardContent style={{paddingLeft:"50px"}}>
-          <Bar barData={this.state.barData} />
+          <CardContent style={{ paddingLeft: "50px" }}>
+            <Bar barData={this.state.barData} />
           </CardContent>
         </Card>
-        <Card style={{width: "40%" ,margin:"2vw"}}>
-          <CardActions  style={{justifyContent:"space-between"}}> 
+        <Card style={{ width: "40%", margin: "2vw" }}>
+          <CardActions style={{ justifyContent: "space-between" }}>
             <p>Total cash flow</p>
-            <div  style={{justifyContent:"space-between",display:"flex"}}>
-            <span style={{justifyContent:"space-between",display:"flex",alignItems:"center",marginRight:"10px"}}>
-              <div style={{height:"10px",width:"10px",borderRadius:"2px",backgroundColor:"green",marginRight:"3px"}}></div>
-              <p>In </p>
-            </span>
-            
-            <span style={{justifyContent:"space-between",display:"flex",alignItems:"center",marginRight:"10px"}}>
-             <div style={{height:"10px",width:"10px",borderRadius:"2px",backgroundColor:"green",marginRight:"3px"}}></div>
-              <p>Out</p>
-            </span>
+            <div style={{ justifyContent: "space-between", display: "flex" }}>
+              <span
+                style={{
+                  justifyContent: "space-between",
+                  display: "flex",
+                  alignItems: "center",
+                  marginRight: "10px",
+                }}
+              >
+                <div
+                  style={{
+                    height: "10px",
+                    width: "10px",
+                    borderRadius: "2px",
+                    backgroundColor: "green",
+                    marginRight: "3px",
+                  }}
+                ></div>
+                <p>In </p>
+              </span>
+
+              <span
+                style={{
+                  justifyContent: "space-between",
+                  display: "flex",
+                  alignItems: "center",
+                  marginRight: "10px",
+                }}
+              >
+                <div
+                  style={{
+                    height: "10px",
+                    width: "10px",
+                    borderRadius: "2px",
+                    backgroundColor: "green",
+                    marginRight: "3px",
+                  }}
+                ></div>
+                <p>Out</p>
+              </span>
             </div>
-            
           </CardActions>
           <hr />
-          <CardContent style={{paddingLeft:"50px"}}>
-          <Bar barData={this.state.barData} />
+          <CardContent style={{ paddingLeft: "50px" }}>
+            <Bar barData={this.state.barData} />
           </CardContent>
         </Card>
-        <Card style={{width: "40%",margin:"2vw"}}>
+        <Card style={{ width: "40%", margin: "2vw" }}>
           <CardActions>
-            <p>Checking Account</p>
-            <span></span>
+            <p>Account watchlist</p>
           </CardActions>
           <hr />
-          <CardContent style={{paddingLeft:"50px"}}>
-          <Line lineData={this.state.lineData} />
+          <CardContent >
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 200 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Account</TableCell>
+                    <TableCell align="right">This month</TableCell>
+                    <TableCell align="right">YTD</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </CardContent>
         </Card>
 
         {/* popup code */}
         <Dialog
-        open={this.state.open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={this.handleClick}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleClick}>Disagree</Button>
-          <Button onClick={this.handleClick}>Agree</Button>
-        </DialogActions>
-      </Dialog>
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClick}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>{"Upload new invoice?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              <input type="file" disabled />
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClick}>Disagree</Button>
+            <Button onClick={this.handleClick}>Agree</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
